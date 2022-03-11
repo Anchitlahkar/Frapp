7b3dd41b2f7bf37f4bfadfe294734c76993e9bbc
Anchit Lahkar <anchitlahkar0202@gmail.com>
2022-03-11 21:13:48 +0530

    11.03.2022

--- App.js
+++ App.js
@@ -1,7 +1,31 @@
+import * as React from "react"
 import Main from "./screens/main";
+import HomeScreen from "./screens/homeScreen";
+import "react-native-gesture-handler"
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+
+const Stack = createStackNavigator()
+
 
 export default function App() {
   return (
-    <Main />
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="HOME" screenOptions={{ headerShown: false }} >
+
+        <Stack.Screen
+          name="HOME"
+          component={HomeScreen}
+        />
+
+        <Stack.Screen
+          name="Main"
+          component={Main}
+        />
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
